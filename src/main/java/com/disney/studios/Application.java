b3c6e0d1f06a815579e746c04570a781df70b704
denisfoconnor1102 <denisfoconnor@gmail.com>
2022-06-26 17:54:31 -0400

    Working Ngrok auto

--- src/main/java/com/disney/studios/Application.java
+++ src/main/java/com/disney/studios/Application.java
@@ -2,6 +2,9 @@ package com.disney.studios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.github.alexdlaird.ngrok.NgrokClient;
+import com.github.alexdlaird.ngrok.protocol.CreateTunnel;
+import com.github.alexdlaird.ngrok.protocol.Tunnel;
 
 /**
  * Bootstraps the Spring Boot com.disney.studios.Application
@@ -12,5 +15,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        setNgrok();
+    }
+
+    private static void setNgrok() {
+        final NgrokClient ngrokClient = new NgrokClient.Builder().build();
+        final CreateTunnel createTunnel = new CreateTunnel.Builder()
+                .withAddr(8080)
+                .build();
+        final Tunnel tunnel = ngrokClient.connect(createTunnel);
     }
 }
